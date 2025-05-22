d1857df327dcce4d4cb2283871c9f3236b4f9352
madamiec19 <madamiec19@gmail.com>
2025-05-22 15:50:13 +0200

    beta version

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeaturesSection from "./components/FeaturesSection";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-    <Header />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeaturesSection />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,57 @@
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        <span className="bg-gradient-to-r from-green-500 to-blue-600 text-transparent bg-clip-text">
+          Twój{" "}
+        </span>
+        Serwis Rowerowy
+      </h1>
+      <p className="mt-10 text-lg text-center text-neutral-500">
+        Niezależnie od tego, czy potrzebujesz szybkiej regulacji, kompleksowego
+        przeglądu, czy specjalistycznej naprawy – jesteśmy tu, by zadbać o Twój
+        rower z pasją i precyzją. Zaufaj ekspertom, dla których dwa kółka to
+        cały świat.
+      </p>
+      <div className="flex justify-center my-10">
+        <a
+          href="#"
+          className="bg-gradient-to-r from-blue-500 to-blue-800 text-white py-3 px-6 rounded-md shadow-lg hover:shadow-xl transition duration-300"
+        >
+          Umów wizytę
+        </a>
+        <a
+          href="#"
+          className="ml-4 py-3 px-6 border rounded-md shadow-lg hover:shadow-xl transition duration-300"
+        >
+          Poznaj ofertę
+        </a>
+      </div>
+      <div className="flex justify-center mt-10 ">
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-blue-700 shadow-sm shadow-blue-400 mx-2 my-4"
+        >
+          <source src={video1} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-blue-700 shadow-sm shadow-blue-400 mx-2 my-4"
+        >
+          <source src={video2} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
